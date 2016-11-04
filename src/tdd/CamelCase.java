772d23ea3ee166534f1433dd7133ce0488d0d31f
“iquee” <luizcomz@gmail.com>
2016-11-04 00:17:37 -0200

    commit inicial

---
+++ src/tdd/CamelCase.java
@@ -0,0 +1,48 @@
+package tdd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CamelCase {
+	
+	private static String pattern = "(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z0-9][a-z0-9])";
+
+	public List<String> converterCamelCase(String palavraTeste) {
+		validacaoInicial(palavraTeste);
+		List<String> lista = new ArrayList<String>();
+		for (String palavra : palavraTeste.split(pattern)) {
+			lista.add(verificaAoAdicionar(palavra));
+		}
+		return lista; 
+	}
+	
+	/**
+	 * Verifica se a palavra comeca com número ou contém algum caracter especial
+	 * @param palavraTeste
+	 * @throws IllegalArgumentException
+	 */
+	private void validacaoInicial(String palavraTeste){
+		String pattern = "^[a-zA-Z][a-zA-Z0-9]*$";		
+		if(!palavraTeste.matches(pattern)){
+			throw new IllegalArgumentException("Palavra não pode iniciar com números ou conter algum caracter especial");
+		}
+	}
+	
+	/**
+	 * Antes de adicionar na lista, verifica se as letras permanecem ou ficam maiúsculas
+	 * @param palavra
+	 * @return String
+	 */
+	private String verificaAoAdicionar(String palavra){
+		if(palavra.length() == 1){
+			return palavra;		
+		} else{
+			if(Character.isUpperCase(palavra.charAt(1))){
+				return palavra;
+			} else{
+				return palavra.toLowerCase();
+			}
+		}
+	}
+
+}
---
+++ src/tdd/TesteCamelCase.java
@@ -0,0 +1,35 @@
+package tdd;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TesteCamelCase {
+	
+	final private static String PALAVRA_TESTE_1 = "recupera10PrimeirosTDDCourseraX";
+	final private static String PALAVRA_TESTE_2 = "10PrimeirosTDDCourseraX"; 
+	final private static String PALAVRA_TESTE_3 = "meuTDD#2016"; 
+	
+	@Test
+	public void testeConverterCamelCase(){
+		CamelCase cm = new CamelCase();
+		List<String> lista = cm.converterCamelCase(PALAVRA_TESTE_1);
+		assertEquals(6, lista.size());
+		assertEquals(Arrays.asList("recupera", "10", "primeiros", "TDD", "coursera", "X"), lista);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testeInvalidoConverterCamelCase(){
+		CamelCase cm = new CamelCase();
+		cm.converterCamelCase(PALAVRA_TESTE_2);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testeCaracterInvalidoConverterCamelCase(){
+		CamelCase cm = new CamelCase();
+		cm.converterCamelCase(PALAVRA_TESTE_3);
+	}
+}
